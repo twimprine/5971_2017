582662e574f1ef8c332f746c95c775c3cacf99d6
Thomas Wimprine <twimprine@seriontechnology.com>
2017-03-11 21:27:36 -0600

    Added camera subsustem

---
+++ src/org/usfirst/frc/team5971/robot/subsystems/Camera.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team5971.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Camera extends Subsystem{
+	public void init(){
+		
+	}
+	
+	public void initDefaultCommand() {
+		
+	}
+}
