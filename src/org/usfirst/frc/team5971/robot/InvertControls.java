b2bf9db3e1d5eb7fca4f272d35d73c4da3ada774
Thomas Wimprine <twimprine@seriontechnology.com>
2017-03-21 19:42:36 -0500

    Added example class InvertControls

---
+++ src/org/usfirst/frc/team5971/robot/InvertControls.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc.team5971.robot;
+
+public class InvertRobot {
+
+boolean isInverted = false;
+
+boolean getStatus {
+    return isInverted;
+}
+
+void setStatus {
+    if (isInverted) {
+        isInverted = false;
+    else 
+        isInverted = true;
+    }
+}
+
+
+}
+
